929c359658c2edf1c2306a76331eb58a1c3fda89
Pleonasmo <pepirian24@gmail.com>
2023-04-26 16:00:23 -0300

    Update dos arquivos necessarios

--- lista-un1/q1/main.cpp
+++ lista-un1/q1/main.cpp
@@ -1,9 +1,10 @@
 #include "funcoes.cpp"
 #include <iostream>
+#include <string.h>
 
 using namespace std;
 
-int main() {
+int main(int argc, char * argv[]) {
   int figura;
   std::cout << "Selecione a figura para qual deseja calcular a área:\n1 - "
                "Quadrado\n2 - Retângulo\n3 - Círculo\n4 - Triângulo\n\n";
@@ -11,7 +12,7 @@ int main() {
   std::cin >> figura;
 
   if (figura == 1) {
-    
+    areaQuadrado(1);
   } else if(figura == 2){
     
   } else if(figura == 3){
