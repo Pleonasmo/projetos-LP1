b279af0782bd5ca459ce5c2562bc72e9ace0eb09
Pleonasmo <49865204+Pleonasmo@users.noreply.github.com>
2023-05-03 13:52:19 +0000

    Adição da opção de escrita no arquivo txt (ainda com pendencias)

--- chiquinho_lanches/gerente.cpp
+++ chiquinho_lanches/gerente.cpp
@@ -7,8 +7,10 @@ using namespace std;
 
 //Função de verificação de credenciais para acesso ao sistema
 bool validarGerente(string usuario, string senha){
-    if(usuario == "admin" && senha == "admin")
+    if(usuario == "admin" && senha == "admin"){
+        cout << "\nAcessando sistema como gerente...\n" << endl;
         return true;
+    }
     else
         return false;
 }
@@ -18,8 +20,8 @@ void acessarGerente(){
     string usuario, senha, texto;
 
     //Interação com o usuário para inserção de credenciais
-    cout << "\nAcessando sistema como gerente...\n" << endl;
-    cout << "Digite o usuario: ";
+    
+    cout << "\nDigite o usuario: ";
     cin >> usuario;
     cout << "Digite a senha: ";
     cin >> senha;
@@ -31,37 +33,54 @@ void acessarGerente(){
         cout << "\nAcesso permitido!" << endl;
       
         int escolha; //int representando escolha do menu do sistema
-        ifstream dados;
+        fstream dados;
         string leitura;
 
       //laço while do menu do sistema do gerente
         while(true){
-          cout << "\nAcessando sistema como gerente...\n" << endl;
-          cout << "Qual das opções abaixo você deseja?\n" << endl;
+          cout << "\nQual das opções abaixo você deseja?\n" << endl;
           cout << "1 - Listar sanduíches cadastrados" << endl;
           cout << "2 - Listar sucos cadastrados" << endl;
-          cout << "3 - Sair do sistema\n" << endl;
+          cout << "3 - Cadastrar novo sanduíche" << endl;
+          cout << "4 - Cadastrar novo suco" << endl;
+          cout << "5 - Sair do sistema\n" << endl;
         
           cin >> escolha;
           
           if(escolha == 1){
             //Leitura do arquivo de sanduiches
             dados.open("sanduiches.txt");
-            cout << "\nSanduíches disponiveis no cardápio: " << endl;
+            cout << "\n------------------------------------" << 
+              "\nSanduíches disponiveis no cardápio: " << endl;
             while(getline(dados, leitura))
               cout << "- " << leitura << endl;
             dados.close();
+            cout << "------------------------------------\n" << endl;
           } else if(escolha == 2){
-            //Leitura do arquivo de sucos
-            dados.open("sucos.txt");
-            cout << "\nSucos disponiveis no cardápio: " << endl;
-            while(getline(dados, leitura))
-              cout << "- " << leitura << endl;
-            dados.close();
+              //Leitura do arquivo de sucos
+              dados.open("sucos.txt");
+              cout << "\n------------------------------------" <<
+              "\nSucos disponiveis no cardápio: " << endl;
+              while(getline(dados, leitura))
+                cout << "- " << leitura << endl;
+              dados.close();
+              cout << "------------------------------------\n" << endl;
           } else if(escolha == 3){
-            //Quebra do laço e saída do sistema
-            cout << "\nSaindo do sistema...\n" << endl;
-            break;
+              string sanduiche;
+              dados.open("sanduiches.txt", ios::app);
+              cout << "\n------------------------------------" <<
+              "\nDigite o sanduiche e o preço (nome-do-sanduiche preço):" << endl;
+              cin.ignore();
+              getline(cin, sanduiche);
+              dados << "\n";
+              dados << sanduiche;
+              cout << "\nSanduíche cadastrado!\n" << endl;
+              dados.close();
+              cout << "------------------------------------\n" << endl;
+          } else if(escolha == 5){
+              //Quebra do laço e saída do sistema
+              cout << "\nSaindo do sistema...\n" << endl;
+              break;
           }
         }
     }
