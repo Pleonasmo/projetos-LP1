3672a8c953100fe2ba177c8fa22083f2e9f6e7d6
Pedro Rian <49865204+Pleonasmo@users.noreply.github.com>
2023-06-11 23:34:03 -0300

    avanços em Empresa.cpp (ainda com demandas)

--- rh_atacado_dos_calcados/Empresa.cpp
+++ rh_atacado_dos_calcados/Empresa.cpp
@@ -1,14 +1,58 @@
 #include "Empresa.hpp"
 
+//Construtor vazio:
 Empresa::Empresa(){
     faturamentoMensal = 0;
     nomeEmpresa = "Empresa";
     cnpj = "00000000000000";
 }
 
+//Construtor parametrizado:
 Empresa::Empresa(float fm, std::string ne, std::string cn){
     faturamentoMensal = fm;
     nomeEmpresa = ne;
     cnpj = cn;
+}
+
+//Getters e Setters:
+float Empresa::getFaturamentoMensal(){
+    return faturamentoMensal;
+}
+
+void Empresa::setFaturamentoMensal(float fm){
+    faturamentoMensal = fm;
+}
+
+std::string Empresa::getNomeEmpresa(){
+    return nomeEmpresa;
+}
+
+void Empresa::setNomeEmpresa(std::string ne){
+    nomeEmpresa = ne;
+}
+
+std::string Empresa::getCnpj(){
+    return cnpj;
+}
+
+void Empresa::setCnpj(std::string cn){
+    cnpj = cn;
+}
+
+//Getters do Dono e dos vetores:
+Pessoa Empresa::getDono(){
+    return dono;
+}
+
+std::vector<Asg> Empresa::getAsgs(){
+    return asgs;
+}
+
+std::vector<Vendedor> Empresa::getVendedores(){
+    return vendedores;
+}
+
+std::vector<Gerente> Empresa::getGerentes(){
+    return gerentes;
 }
 
