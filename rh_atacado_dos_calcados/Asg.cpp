03e143c430c8de0d4f16313b34119f75bb4bd669
Pedro Rian <49865204+Pleonasmo@users.noreply.github.com>
2023-06-13 19:20:23 -0300

    calcularRecisao() de Asg implementado

--- rh_atacado_dos_calcados/Asg.cpp
+++ rh_atacado_dos_calcados/Asg.cpp
@@ -47,5 +47,35 @@ float Asg::calcularSalario(int diasFaltas){
 }
 
 float Asg::calcularRecisao(Data desligamento){
-    return 1;
+    float salario_asg;
+    try{
+        salario_asg = stof(getSalario());
+    } catch(const std::invalid_argument e){
+        std::cout << "O salario passado é inválido!" << endl;
+        salario_asg = 0;
+    } catch(const std::out_of_range e){
+        std::cout << "O salario passado é inválido!" << endl;
+        salario_asg = 0;
+    }
+
+    //Calculo de dias trabalhados na empresa:
+    Data dataInicio = getIngressoEmpresa();
+    Data dataFim = desligamento;
+
+    int diferencaAnos = dataFim.ano - dataInicio.ano;
+    int diferencaMeses = dataFim.mes - dataInicio.mes;
+    int diferencaDias = dataFim.dia - dataInicio.dia;
+
+    if(diferencaMeses < 0){
+        diferencaMeses += 12;
+    } if(diferencaDias < 0){
+        diferencaDias += 12;
+    }
+
+    int diasTrabalhados = diferencaAnos*360 + diferencaMeses*30 + diferencaDias;
+
+    //Calculo da recisão:
+    float recisao = salario_asg * diasTrabalhados/360;
+
+    return recisao;
 }
