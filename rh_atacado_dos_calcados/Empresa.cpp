5a0b4b5a5c9347e5bf9cd1cd233bb6548b2b189b
Pedro Rian <49865204+Pleonasmo@users.noreply.github.com>
2023-06-13 22:21:08 -0300

    carregarFuncoes() de Empresa e correção de erros

--- rh_atacado_dos_calcados/Empresa.cpp
+++ rh_atacado_dos_calcados/Empresa.cpp
@@ -1,3 +1,4 @@
+#include <fstream>
 #include "Empresa.hpp"
 
 //Construtor vazio:
@@ -56,3 +57,106 @@ std::vector<Gerente> Empresa::getGerentes(){
     return gerentes;
 }
 
+//Outros metodos de Empresa:
+void Empresa::carregarFuncoes(){
+    try{
+        std::ifstream arquivo("dados/funcoes.txt");
+        if (!arquivo) {
+            throw std::runtime_error("Erro ao abrir o arquivo.");
+        } else {
+            std::cout << "Encontrou o arquivo" << std::endl;
+            std::string linha;
+            while (std::getline(arquivo, linha)) {
+                if (linha == "carregarEmpresa()") {
+                    carregarEmpresa();
+                } else if (linha == "carregarAsg()") {
+                    carregarAsg();
+                } else if (linha == "carregarVendedor()") {
+                    carregarVendedor();
+                } else if (linha == "carregarGerente()") {
+                    carregarGerente();
+                } else if (linha == "carregarDono()") {
+                    carregarDono();
+                } else if (linha == "imprimeAsgs()") {
+                    imprimeAsgs();
+                } else if (linha == "imprimeVendedores()") {
+                    imprimeVendedores();
+                } else if (linha == "imprimeGerentes()") {
+                    imprimeGerentes();
+                } else if (linha == "imprimeDono()") {
+                    imprimeDono();
+                } else if (linha == "buscaFuncionario()") {
+                    std::cout << "FUNCAO NAO IMPLEMENTADA" << std::endl;
+                } else if (linha == "calculaSalarioFuncionario()") {
+                    std::cout << "FUNCAO NAO IMPLEMENTADA" << std::endl;
+                } else if (linha == "calculaTodosOsSalarios()") {
+                    calculaTodosOsSalarios();
+                } else if (linha == "calcularRecisao()") {
+                    std::cout << "FUNCAO NAO IMPLEMENTADA" << std::endl;
+                } else {
+                    std::cerr << "Função desconhecida: " << linha << std::endl;
+                }
+            }
+        }
+
+        arquivo.close();
+    } catch(const std::exception& e){
+        std::cerr << "Erro: " << e.what() << std::endl;
+    }
+    
+
+
+    
+}
+
+void Empresa::carregarEmpresa(){
+    std::cout << "carregarEmpresa" << std::endl;
+}
+
+void Empresa::carregarAsg(){
+    std::cout << "carregarAsg" << std::endl;
+}
+
+void Empresa::carregarVendedor(){
+    std::cout << "carregarVendedor" << std::endl;
+}
+
+void Empresa::carregarGerente(){
+    std::cout << "carregarGerente" << std::endl;
+}
+
+void Empresa::carregarDono(){
+    std::cout << "carregarDono" << std::endl;
+}
+
+void Empresa::imprimeAsgs(){
+    std::cout << "imprimeAsgs" << std::endl;
+}
+
+void Empresa::imprimeVendedores(){
+    std::cout << "imprimeVendedores" << std::endl;
+}
+
+void Empresa::imprimeGerentes(){
+    std::cout << "imprimeGerentes" << std::endl;
+}
+
+void Empresa::imprimeDono(){
+    std::cout << "imprimeDono" << std::endl;
+}
+
+void Empresa::buscaFuncionario(int matricula){
+    std::cout << "buscaFuncionario" << std::endl;
+}
+
+void Empresa::calculaSalarioFuncionario(int matricula){
+    std::cout << "calculaSalarioFuncionario" << std::endl;
+}
+
+void Empresa::calculaTodosOsSalarios(){
+    std::cout << "calculaTodosOsSalarios" << std::endl;
+}
+
+void Empresa::calcularRecisao(int matricula, Data desligamento){
+    std::cout << "calcularRecisao" << std::endl;
+}
--- rh_atacado_dos_calcados/main.cpp
+++ rh_atacado_dos_calcados/main.cpp
@@ -8,6 +8,7 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
     Empresa *atacado_dos_alcados = new Empresa(156289.56, "Atacado dos Calçados", "40.101.588/0001-98");
+    atacado_dos_alcados->carregarFuncoes();
     cout << "Deu bom!" << endl;
     return 0;
 }
